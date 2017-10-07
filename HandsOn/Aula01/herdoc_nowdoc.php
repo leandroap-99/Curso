44aa32122c2270f59834ca065a802b68588aeab8
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2017-10-07 14:01:27 -0300

    segundo

---
+++ HandsOn/Aula01/herdoc_nowdoc.php
@@ -0,0 +1,13 @@
+<?php
+$certi = "Zend";
+$curso = <<< TEXTO
+
+Estou me preparando para a certificacao $certi na 4linux;
+
+TEXTO;
+echo $curso;
+echo "<br>";
+$curso= <<< 'TEXTO'
+Estou me preparando para a certificacao $certi na 4linux;
+TEXTO;
+echo $curso;
